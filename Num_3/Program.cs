b1c53a4cbbe4869ea8bb41d7c04ec6f364d6e3e7
besser <besser9009@mail.ru>
2023-04-10 02:22:30 +0300

    add Num_3 and redact Num_1 & Num_2

---
+++ Num_3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+int []Array(int length)
+{
+    int []array = new int[length];
+    for (int index = 0; index < length; index++)
+    {
+        array[index] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+void Print( int []array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; ++i)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    Console.WriteLine("]");
+}
+int length = 8;
+int []array = Array(length);
+Print(array);
