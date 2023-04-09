0bab09507cb69892a865241e7df2f27b7cffb0d5
besser <besser9009@mail.ru>
2023-04-10 01:09:17 +0300

    add Num_2

---
+++ Num_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int num = 0;
+int sum = 0;
+while (number > 0)
+{
+    sum += number % 10;
+    number /= 10;
+}
+System.Console.WriteLine(sum);
